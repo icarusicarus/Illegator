0366ee1b1fa52a0de94ce57704e1b03d6ee177b2
icaruswithoutwings <bluemonster@pusan.ac.kr>
2021-01-14 09:53:25 -0800

    [UPDATE] crawl link, take a screenshot

--- crawler.py
+++ crawler.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+from urllib.parse import urlparse
 from selenium import webdriver
 from collections import Counter
 from konlpy.tag import Hannanum
@@ -7,42 +8,75 @@ import pytagcloud
 import webbrowser
 import random
 import sys
+import os
 
-url = 'http://xnuna.me/'
+class url_struct:
+    def __init__(self, internal, url, depth):
+        self.internal = internal    # internal: 1, external: 0
+        self.url = url
+        self.depth = depth
+
+url_list = []
+
+internal_depth = 3
+external_depth = 5
+
 r = lambda: random.randint(0,255)
 color = lambda: (r(), r(), r())
 
-
 def get_tags(text, ntags=50, multiplier=10):
-    print("1")
     h = Hannanum()
     nouns = h.nouns(text)
     count = Counter(nouns)
     print(count)
-    print("2")
     return [{'color': color(), 'tag': n, 'size': c*multiplier}\
         for n, c in count.most_common(ntags)]
 
 def draw_cloud(tags, filename, fontname='Cantarell', size=(800, 600)):
-    print("3")
     pytagcloud.create_tag_image(tags, filename, fontname=fontname, size=size)
     webbrowser.open(filename)
-    print("4")
 
-def crawler():
-    driver = webdriver.PhantomJS(executable_path="./phantomjs")
-    driver.implicitly_wait(10)
-    driver.get(url)
-    a = driver.find_elements_by_tag_name("a")
-    count =0
-    html = driver.page_source
-    soup = BeautifulSoup(html, 'html.parser')
-    text = soup.get_text()
-    #print(text)
-    tags = get_tags(text)
-    draw_cloud(tags, 'wordcloud.png')
+def crawler(struct):
+    while(len(url_list)):
+        if((struct.internal and (struct.depth < internal_depth)) or (not struct.internal and (struct.depth < external_depth))):
+            driver = webdriver.PhantomJS(executable_path="./phantomjs")
+            driver.implicitly_wait(3)
+            driver.get(struct.url)
+            print("[O] Crawling " + struct.url + "...")
+            html = driver.page_source
+            soup = BeautifulSoup(html, 'html.parser')
 
-    driver.quit()
+            # get internal/external link and append to url list            
+            net = urlparse(struct.url).netloc
+
+            hrefs = soup.findAll("a")
+            for href in hrefs:
+                if href.attrs["href"].startswith('/'):                                   # internal link start with /
+                    ap = url_struct(1, str(net + href.attrs["href"]), struct.depth + 1)
+                    url_list.append(ap)
+                    print("[-] Find Internal Link: " + href.attrs["href"])
+                elif (urlparse(href.attrs["href"]).netloc == net):                      # internal link
+                    ap = url_struct(1, str(href.attrs["href"]), struct.depth + 1)
+                    url_list.append(ap)
+                    print("[-] Find Internal Link: " + href.attrs["href"])
+                else:                                                                   # external link
+                    ap = url_struct(0, str(href.attrs["href"]), struct.depth + 1)
+                    url_list.append(ap)
+                    print("[=] Find External Link: " + href.attrs["href"])
+            
+            driver.save_screenshot('./screenshot/' + net + '.png')
+            print("[+] Take a Screenshot. Crawled Complete. Delete " + struct.url)
+
+            del url_list[0]
+
+        exit()  # for test. i will expand to continuos crawling
+
+            #text = soup.get_text()
+            #tags = get_tags(text)
+            #draw_cloud(tags, 'wordcloud.png')
 
 if __name__ == "__main__":
-    crawler()
+    start_struct = url_struct(0, "https://http.cat/", 0)
+    url_list.append(start_struct)
+
+    crawler(start_struct)
